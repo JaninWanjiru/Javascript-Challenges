f23b291e00402e82edb18e4ecad10ca247b26ded
JaninWanjiru <janinndongo02@gmail.com>
2025-05-21 00:31:10 +0300

    Completed Challenge 2: Convert Minutes into Seconds

--- milestone_1_challenges.js
+++ milestone_1_challenges.js
@@ -4,8 +4,19 @@
 function addition(a, b) {
   console.log(a + b);
 }
+
 addition(-10, 25);
 addition(8, -12);
+
+// Challenge 2: Convert Minutes into Seconds
+// Write a function called convert that takes an integer minutes and converts it to seconds.
+
+function convert(minutes) {
+    const seconds = minutes * 60;
+    return seconds;
+}
+
+console.log(convert(25));
 
 
 
