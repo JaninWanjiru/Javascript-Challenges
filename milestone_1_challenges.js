85727d228d7b554507d7b2ae86a6214c77c7a371
JaninWanjiru <janinndongo02@gmail.com>
2025-05-21 00:51:43 +0300

    Completed challenge 3: Perimeter of a Rectangle

--- milestone_1_challenges.js
+++ milestone_1_challenges.js
@@ -1,16 +1,14 @@
 // Challenge 1: Return the sum of two numbers
 // Create a function called addition that takes two numbers as arguments and returns their sum.
-
 function addition(a, b) {
   console.log(a + b);
 }
 
 addition(-10, 25);
-addition(8, -12);
+
 
 // Challenge 2: Convert Minutes into Seconds
 // Write a function called convert that takes an integer minutes and converts it to seconds.
-
 function convert(minutes) {
     const seconds = minutes * 60;
     return seconds;
@@ -18,6 +16,15 @@ function convert(minutes) {
 
 console.log(convert(25));
 
+
+// Challenge 3: Perimeter of a Rectangle
+// Create a function that takes length and width and finds the perimeter of a rectangle.
+function findPerimeter(length, width) {
+    let perimeter = 2 * (length + width);
+    return perimeter;
+}
+
+console.log(findPerimeter(15, 30));
 
 
 
