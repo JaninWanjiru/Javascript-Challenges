4a47ba717f0b7d8237dc9ba083acb63f3d308c5a
JaninWanjiru <janinndongo02@gmail.com>
2025-05-21 00:11:04 +0300

    completed challenge 1: Sum of two numbers

---
+++ milestone_1_challenges.js
@@ -0,0 +1,15 @@
+// Challenge 1: Return the sum of two numbers
+// Create a function called addition that takes two numbers as arguments and returns their sum.
+
+function addition(a, b) {
+  console.log(a + b);
+}
+addition(-10, 25);
+addition(8, -12);
+
+
+
+
+
+
+
