da6b392ac6f283b06f9d3aa2c082b381a457b6e0
JaninWanjiru <janinndongo02@gmail.com>
2025-05-22 10:16:22 +0300

    completed challenge 2: Find Maximum value

--- milestone_2_challenges.js
+++ milestone_2_challenges.js
@@ -10,3 +10,16 @@ function sumOfPositives(numbers) {
 }
 console.log(sumOfPositives([1, -4, 7, 12])); 
 console.log(sumOfPositives([1, -3, 5, -2, 9, -8]));   
+
+
+// Challenge 2: Find Maximum Value
+function findMax(array) {
+    let max = array[0];
+  for (let i = 1; i < array.length; i++) {
+    if (array[i] > max) {
+      max = array[i];
+    }
+  }
+  return max;
+}
+console.log(findMax([3, 7, 2, 9, 4]));
