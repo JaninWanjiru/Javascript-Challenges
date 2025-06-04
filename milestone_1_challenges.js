9c13d25a5eb8fc2b8af7b28e2af7827c54ca4b6b
JaninWanjiru <janinndongo02@gmail.com>
2025-06-04 17:46:10 +0300

    update some changes

--- milestone_1_challenges.js
+++ milestone_1_challenges.js
@@ -1,7 +1,7 @@
 // Challenge 1: Return the sum of two numbers
 // Create a function called addition that takes two numbers as arguments and returns their sum.
 function addition(a, b) {
-  console.log(a + b);
+  return(a + b);
 }
 
 addition(-10, 25);
@@ -37,22 +37,22 @@ function isNegative(number) {
   }
 }
 
-console.log(isNegative(25));
-console.log(isNegative(-5));
+console.log(isNegative(-23));
+console.log(isNegative(55));
 
 
 // Challenge 5: Can I drive
 // Write a function that takes in a person's name and age. If they're 18 or older, return a message like 'Alice is old enough to drive.' If they're younger, return 'Bob is not old enough to drive yet.'
 function canDrive(name, age) {
    if (age >= 18) {
-     console.log(`${name} is old enough to drive`);
+     return `${name} is old enough to drive`;
    } else {
-     console.log(`${name} is not old enough to drive yet`);
+     return `${name} is not old enough to drive yet`;
    }
  }
 
-canDrive("Becky", 22);
-canDrive("Jack", 15);
+canDrive("Jane", 22);
+canDrive("June", 12);
 
 
 // Challenge 6: Largest Number
@@ -67,8 +67,7 @@ function findLargest(a, b, c) {
   }
 }
 
-console.log(findLargest(10, 20, 30));
-console.log(findLargest(70, 90, 50));
+console.log(findLargest(5, 9, 3));
 
 
 // Challenge 7: BMI Calculator
@@ -85,8 +84,8 @@ function calculateBMI(weight, height) {
     return "Obese";
   }
 }
-console.log(`Your BMI is - ${calculateBMI(48.50, 1.5)}`);
-console.log(`Your BMI is -  ${calculateBMI(58, 1.5)}`);
+console.log(`Your BMI is - ${calculateBMI(68, 1.75)}`);
+console.log(`Your BMI is - ${calculateBMI(85, 1.8)}`);
 
 
 // Challenge 8: Greeting based on time
@@ -99,17 +98,15 @@ function greetUser(name, hour) {
     return `Good afternoon, ${name}!`;
   } else if (hour >= 18 && hour <= 21) {
     return `Good evening, ${name}!`;
-  } else if (hour >= 22 && hour <= 24) {
-    return `Good night, ${name}!`;
   } else {
-    return `hour not valid!`;
-  }
+    return `Good night, ${name}!`;
+  } 
 }
 
-console.log(greetUser("Kelly", 7));
-console.log(greetUser("Benson", 15));
-console.log(greetUser("Hope", 20));
-console.log(greetUser("Kabi", 23));
+console.log(greetUser("Alice", 9));
+console.log(greetUser("Alice", 15));
+console.log(greetUser("Alice", 20));
+console.log(greetUser("Alice", 1));
 
 
 // Challenge 9: FizzBuzz
@@ -129,9 +126,9 @@ function fizzBuzzCheck(number) {
     return number;
   }
 }
-console.log(fizzBuzzCheck(9));
+console.log(fizzBuzzCheck(3));
 console.log(fizzBuzzCheck(10));
-console.log(fizzBuzzCheck(30));
+console.log(fizzBuzzCheck(15));
 console.log(fizzBuzzCheck(7));
 
 
@@ -143,18 +140,18 @@ function perimeter2(letter, num) {
   } else if (letter === "c") {
     return num * 6.28;
   } else {
-    return "Wrong input!";
+    return number;
   }
 }
-console.log(perimeter2("s", 10));
-console.log(perimeter2("c", 2));
+console.log(perimeter2("s", 7));
+console.log(perimeter2("c", 4));
 
 
 // Challenge 11: Sum of Even Numbers
 // Write a function called sumEvenNumbers(n) that takes a number n and returns the sum of all even numbers from 1 up to and including n.
 function sumEvenNumbers(n) {
   let sum = 0;
-  for (let i = 0; i <= n; i++) {
+  for (let i = 2; i <= n; i++) {
     if (i % 2 === 0) {
       sum += i;
     }
@@ -162,28 +159,26 @@ function sumEvenNumbers(n) {
   return sum;
 }
 
-console.log(sumEvenNumbers(10));
-console.log(sumEvenNumbers(20));
-console.log(sumEvenNumbers(25));
+console.log(sumEvenNumbers(6));
 
 
 // Challenge 12: Multiply by Itself
 // Write a function called powerUp(num, times) that takes two numbers and returns the result of multiplying num by itself times times using only a loop and arithmetic operators (no Math.pow() allowed).
 function powerUp(num, times) {
-  let power = num ** times;
-  return power;
+  let result = 1;
+  for (let i=1; i<=times; i++){
+    result = result*num;
+  }
+  return result;
 }
 
-console.log(powerUp(8, 0));
-console.log(powerUp(4, 9));
+console.log(powerUp(2, 3));
+console.log(powerUp(5, 0));
 
 
 // Challenge 13: Factorial Calculator
 // Write a function called factorial(n) that takes a non-negative integer n and returns the factorial of n — that’s the product of all positive integers from 1 up to n. By definition, factorial of 0 is 1.
 function factorial(n) {
-  if (n < 0) {
-    return "Invalid input";
-  }
   let result = 1;
   for (let i = 1; i <= n; i++) {
     result *= i;
@@ -192,8 +187,7 @@ function factorial(n) {
 }
 
 console.log(factorial(4));
-console.log(factorial(2));
-console.log(factorial(0));
+
 
 
 // Challenge 14: Multiple Sum
@@ -201,15 +195,15 @@ console.log(factorial(0));
 function sumMutiples(n, divisor) {
   let sum = 0;
   for (let i = 1; i <= n; i++) {
-    if (i % divisor == 0) {
-      sum += i;
+    if (i % divisor === 0) {
+      sum = sum + i;
     }
   }
   return sum;
 }
 
-console.log(sumMutiples(15, 10));
-console.log(sumMutiples(7, 5));
+console.log(sumMutiples(10, 2));
+
 
 
 // Challenge 15: Sum of Digits
@@ -223,5 +217,4 @@ function sumDigits(num) {
     return sum;
 }
 
-console.log(sumDigits(450));
-console.log(sumDigits(202));
+console.log(sumDigits(123));
